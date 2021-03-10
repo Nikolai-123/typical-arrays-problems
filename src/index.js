37bf6be6135806ff24dc201dacd3e99cac46e4d2
Nikolai <vova1988open@gmail.com>
2021-03-10 17:34:12 +0300

    feat: add 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
+  if(array === undefined || null || array.length==0){
   return 0;
+}else {
+  array.sort(function(a,b){
+    return a-b;
+  })
+  return array[0];
+}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || null || array.length==0){
+    return 0;
+  }else {
+    array.sort(function(a,b){
+      return b-a;
+    })
+    return array[0];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || null || array.length==0){
+    return 0;
+  } else{
+  let summ =0;
+  for(let i = 0; i <array.length; i++){
+  summ +=array[i] 
+  }
+  return (summ/array.length)
+
+  }
 }
